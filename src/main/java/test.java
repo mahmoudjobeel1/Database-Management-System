ceaac098d586328a456f4597c2d4b936395717d1
mahmoudjobeel1 <mahmoudjobeel@gmail.com>
2021-06-04 07:17:31 +0200

    new

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 import java.text.ParseException;
 import java.util.*;
 
-public class Essam {
+public class test {
     public static void main(String[] args) {
 
     }
